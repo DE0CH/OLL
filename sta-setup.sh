5e97ed10ea4641e4e12c7aa92986c1ba1a620143
Deyao Chen <chendeyao000@gmail.com>
2022-04-13 23:55:51 +0800

    t

---
+++ sta-setup.sh
@@ -0,0 +1,4 @@
+curl -O https://repo.anaconda.com/archive/Anaconda3-2021.11-Linux-x86_64.sh
+sh Anaconda3-2021.11-Linux-x86_64.sh -b -p ~/anaconda3
+~/anaconda3/bin/conda init
+~/anaconda3/bin/conda install -y R

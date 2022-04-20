ddd971160e9a66f919b746d19da4dea2ee4701c8
Deyao Chen <chendeyao000@gmail.com>
2022-04-20 16:45:14 +0800

    add maker

--- maker.py
+++ maker.py
@@ -1,28 +1,38 @@
 import subprocess
 import os
-
-sizes = [10, 100, 1000]
+import sys
 output_f = open("output.txt", "w")
 
-for size in sizes:
-    with open("Instances/1.txt", "w") as f:
-        f.write(f"{size}\n")
-    
-    with open("parameters.txt", "w") as f:
-        for i in range(size):
-            f.write(f"lbd{i} \"--lbd{1} \" i (1, {size-1}) \n")
-    with open("scenario.txt", "w") as f:
-        f.write(f"maxExperiments = {size * 100}")
-    process = subprocess.Popen(["irace", "--parallel", "12"], stdout=subprocess.PIPE)
+def main():
+    sizes = [10, 100, 1000]
 
-    while True:
+    for size in sizes:
+        with open("Instances/1.txt", "w") as f:
+            f.write(f"{size}\n")
+        
+        with open("parameters.txt", "w") as f:
+            for i in range(size):
+                f.write(f"lbd{i} \"--lbd{1} \" i (1, {size-1}) \n")
+        with open("scenario.txt", "w") as f:
+            f.write(f"maxExperiments = {size * 100}")
+        process = subprocess.Popen(["irace", "--parallel", "12"], stdout=subprocess.PIPE)
+
+        while True:
+            try:
+                line = next(process.stdout)
+                line = line.decode("UTF-8")
+                print(line, end="")
+                output_f.write(line)
+            except StopIteration:
+                break
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        output_f.close()
         try:
-            line = next(process.stdout)
-            line = line.decode("UTF-8")
-            print(line, end="")
-            output_f.write(line)
-        except StopIteration:
-            break
-    break
-
-output_f.close()
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
+    output_f.close()

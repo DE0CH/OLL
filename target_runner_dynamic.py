369062045d990bbdb6e91a6e982fc1c6588f3e8f
Deyao Chen <chendeyao000@gmail.com>
2022-05-08 18:24:59 +0800

    add target runner

---
+++ target_runner_dynamic.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3 
+
+import sys
+from onell_algs import onell_lambda
+
+seed = int(sys.argv[3])
+instance = sys.argv[4]
+bound = int(sys.argv[5])
+with open(instance) as f:
+    n = int(f.read())
+
+lbds = [1] * n
+for i in range(n):
+    j = int(sys.argv[i*2+6].replace("--lbd", ""))
+    lbd = int(sys.argv[i*2+7])
+    lbds[j] = lbd
+
+a, b, c = onell_lambda(n, lbds=lbds, max_evals=bound, seed=seed)
+print(c)
+print(c)

cba6fd0f747f8050ccb49273de51ab5e87173862
Deyao Chen <chendeyao000@gmail.com>
2022-05-08 18:21:48 +0800

    update target runner

--- target_runner_static.py
+++ target_runner_static.py
@@ -5,12 +5,14 @@ from onell_algs import onell_lambda
 
 seed = int(sys.argv[3])
 instance = sys.argv[4]
+bound = int(sys.argv[5])
 with open(instance) as f:
     n = int(f.read())
 
-for i in range(1):
-    lbd = int(sys.argv[i*2+6])
-    lbds = [lbd]*n
 
-a, b, c = onell_lambda(n, lbds=lbds, seed=seed)
+lbd = int(sys.argv[7])
+lbds = [lbd]*n
+
+a, b, c = onell_lambda(n, lbds=lbds, max_evals=bound, seed=seed)
+print(c)
 print(c)

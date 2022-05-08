7df7e67fdaf05080e4028f714aa2135a05178f73
Deyao Chen <chendeyao000@gmail.com>
2022-05-08 18:23:40 +0800

    add config

---
+++ config.py
@@ -0,0 +1,22 @@
+SMALL = False
+N = 2
+
+trials = 500 
+threads = 15
+seed = 16950281577708742744
+
+sizes = [
+  10, 
+  50, 
+  100,
+]
+
+if not SMALL:
+  experiment_multiples_dynamic = [10**4] * N
+else:
+  experiment_multiples_dynamic = [50, 30, 20]
+
+if not SMALL:
+  experiment_multiples_static = [100] * N
+else:
+  experiment_multiples_static = [50, 30, 20]

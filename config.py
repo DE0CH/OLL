8a65a90f5b32893688a7765effc41c59ed43bdb3
Deyao Chen <chendeyao000@gmail.com>
2022-05-10 01:07:30 +0800

    lots of changes

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import multiprocessing
 
 SMALL = False
-N = 3
+N = 6
 
 trials = 500 
 threads = int(multiprocessing.cpu_count() * 1.5)
@@ -11,14 +11,31 @@ sizes = [
   10, 
   50, 
   100,
+  200, 
+  500,
+  1000
 ]
 
 if not SMALL:
-  experiment_multiples_dynamic = [10**4] * N
+  experiment_multiples_dynamic = [
+    10**4, 
+    10**4,
+    10**4,
+    5000,
+    2000,
+    1000,
+  ] 
 else:
-  experiment_multiples_dynamic = [50, 30, 20]
+  experiment_multiples_dynamic = [
+    50, 
+    30, 
+    20, 
+    10,
+    10,
+    10
+  ]
 
 if not SMALL:
   experiment_multiples_static = [100] * N
 else:
-  experiment_multiples_static = [50, 30, 20]
+  experiment_multiples_static = [50, 30, 20, 10, 10, 10]
--- probe.py
+++
@@ -1,8 +0,0 @@
-from multiprocessing import Pool
-
-def f(x, y):
-    return x*y
-
-if __name__ == '__main__':
-    with Pool(5) as p:
-        print(p.starmap(f, zip([1, 2, 3], [1, 2, 4])))

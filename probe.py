4dd3f544e0ad5ea8228dc2e7229991ec73979b01
Deyao Chen <chendeyao000@gmail.com>
2022-05-18 21:56:56 +0800

    fix bug

--- probe.py
+++ probe.py
@@ -9,4 +9,4 @@ args = parser.parse_args()
 pathlib.Path("test").mkdir(exist_ok=True, parents=True)
 with open(f"test/{args.n}.txt", 'w') as f:
     f.write(args.n + '\n')
-    f.write(os.cpu_count())
+    f.write(str(os.cpu_count()))

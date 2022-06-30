caa0d7ebbbab811a2836d27dc920dbd1c6cd1f8e
Deyao Chen <chendeyao000@gmail.com>
2022-06-30 14:08:28 +0800

    add grapher error handling

--- irace_grapher_full.py
+++ irace_grapher_full.py
@@ -1,11 +1,15 @@
 import irace_grapher
 import argparse
-from config import N
+from config import N, sizes
+import sys
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser("Graph graphs based on the cached json files")
   parser.add_argument('graphs_path', default='irace_output')
   args = parser.parse_args()
   for i in range(N):
-    irace_grapher.main(i, -1, -1, -1, -1, args.graphs_path)
+    try:
+      irace_grapher.main(i, -1, -1, -1, -1, args.graphs_path)
+    except:
+      print(f"Could not graph with n = {sizes[i]}, most likely because the json data file does not exist", file=sys.stderr) 
 

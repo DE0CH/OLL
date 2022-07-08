73ac25b58a4d63f98eea24ce235f46b992c7b522
Deyao Chen <chendeyao000@gmail.com>
2022-07-08 14:53:46 +0800

    add command line option for binning

--- irace_load_distributor.py
+++ irace_load_distributor.py
@@ -6,11 +6,18 @@ from config import seed, N
 import numpy
 import argparse
 import os
+from enum import Enum 
 
 current_computer = 1
 lock = threading.Lock()
 rng = numpy.random.default_rng(seed)
 mock = False 
+
+class JobType(Enum):
+  baseline = 'baseline'
+  binning = 'binning'
+  def __str__(self):
+    return self.value
 
 def run_job(s):
   s = f'docker build -t irace . && docker run --rm {("--env SMALL="+os.getenv("SMALL") + " ") if os.getenv("SMALL") else ""}-v {"/home/dc262" if not mock else os.getcwd()}:/usr/app irace ' + s
@@ -46,22 +53,27 @@ def run_job(s):
           print(f"Finished running on {name}") 
       subprocess.run(['rsync', '-azvP', f'{name}:/home/dc262/OLL/', '.'], stdout=subprocess.DEVNULL)
       
-def run_full(i, dynamic_seed, dynamic_bin_seed, static_seed, grapher_seed):
-  dynamic_run = Thread(target=run_job, args=(f'python3 irace_dynamic.py {i} {dynamic_seed}',))
-  dynamic_bin_run = Thread(target=run_job, args=(f"python3 irace_dynamic_bin.py {i} {dynamic_bin_seed}",))
-  static_run = Thread(target=run_job, args=(f"python3 irace_static.py {i} {static_seed}",))
-  grapher_run = Thread(target=run_job, args=(f"python3 irace_grapher.py {i} {dynamic_seed} {dynamic_bin_seed} {static_seed} {grapher_seed}", ))
-  dynamic_run.start()
-  dynamic_bin_run.start()
-  static_run.start()
-  dynamic_run.join()
-  dynamic_bin_run.join()
-  static_run.join()
-  grapher_run.start()
-  grapher_run.join()
+def run_full(i, dynamic_seed, dynamic_bin_seed, static_seed, grapher_seed, job_type: JobType):
+  if job_type == JobType.baseline:
+    dynamic_run = Thread(target=run_job, args=(f'python3 irace_dynamic.py {i} {dynamic_seed}',))
+    dynamic_bin_run = Thread(target=run_job, args=(f"python3 irace_dynamic_bin.py {i} {dynamic_bin_seed}",))
+    static_run = Thread(target=run_job, args=(f"python3 irace_static.py {i} {static_seed}",))
+    grapher_run = Thread(target=run_job, args=(f"python3 irace_grapher.py {i} {dynamic_seed} {dynamic_bin_seed} {static_seed} {grapher_seed}", ))
+    dynamic_run.start()
+    dynamic_bin_run.start()
+    static_run.start()
+    dynamic_run.join()
+    dynamic_bin_run.join()
+    static_run.join()
+    grapher_run.start()
+    grapher_run.join()
+  elif job_type == JobType.binning:
+    pass
+  else:
+    raise NotImplemented()
   
-def main():
-  runs = [Thread(target=run_full, args=(i, rng.integers(1<<15, (1<<16)-1), rng.integers(1<<15, (1<<16)-1), rng.integers(1<<15, (1<<16)-1), rng.integers(1<<15, (1<<16)-1))) for i in range(N)]
+def main(job_type: JobType):
+  runs = [Thread(target=run_full, args=(i, rng.integers(1<<15, (1<<16)-1), rng.integers(1<<15, (1<<16)-1), rng.integers(1<<15, (1<<16)-1), rng.integers(1<<15, (1<<16)-1), job_type)) for i in range(N)]
   for thread in runs:
     thread.start()
   for thread in runs:
@@ -69,9 +81,10 @@ def main():
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
+  parser.add_argument('job_type', type=JobType, choices=list(JobType), default=JobType.baseline, nargs='?')
   parser.add_argument('--mock', action="store_true")
   args = parser.parse_args()
   mock = args.mock
-  main()
+  main(args.job_type)
 
 

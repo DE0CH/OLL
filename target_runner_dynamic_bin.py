28835ea1a6f7d6ba122a3ae7c6dfbe6009536db9
Deyao Chen <chendeyao000@gmail.com>
2022-07-19 11:31:46 +0800

    fix bug

--- target_runner_dynamic_bin.py
+++ target_runner_dynamic_bin.py
@@ -1,8 +1,8 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3 
 
 import sys
 from onell_algs_rs import onell_lambda
-from config import suppress_stderr
+from config import get_bins, suppress_stderr
 
 seed = int(sys.argv[3])
 instance = sys.argv[4]
@@ -10,11 +10,19 @@ bound = int(float(sys.argv[5]))
 with open(instance) as f:
   n = int(f.read())
 
+bins, bin_lookup = get_bins(n)
 
-lbd = float(sys.argv[7])
-lbds = [lbd]*n
+lbds = [1] * n
+lbd_bins = [1] * len(bins)
+for i in range(len(bins)):
+  j = int(sys.argv[i*2+6].replace("--lbd", ""))
+  lbd = float(sys.argv[i*2+7])
+  lbd_bins[j] = lbd
+
+for i in range(n):
+  lbds[i] = lbd_bins[bin_lookup[i]]
 
 with suppress_stderr():
-  c = onell_lambda(n, lbds, seed, bound)
+  c = onell_lambda(n, lbds=lbds, max_evals=bound, seed=seed)
 print(c)
 print(c)

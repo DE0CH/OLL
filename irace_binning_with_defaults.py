b83262fba88d40d1cbdc3f92bf16af4de6f290f0
Deyao Chen <chendeyao000@gmail.com>
2022-10-20 10:53:40 +0100

    bug fixes

--- irace_binning_with_defaults.py
+++ irace_binning_with_defaults.py
@@ -1,6 +1,6 @@
 from irace_launcher import IraceCallerBinningWithDefaults
 import argparse
-from config import experiment_multiples_dynamic, load_or_run_binning_comparison_validation, trials, threads, iterative_seeding_iterations, iterative_seeding_sizes, iterative_seeding_multiples, get_iter_bins
+from config import load_or_run_binning_comparison_validation, trials, threads, iterative_seeding_iterations, iterative_seeding_sizes, iterative_seeding_multiples, get_iter_bins
 import json
 from irace_grapher import next_randoms
 import numpy
@@ -17,12 +17,12 @@ def main(i, tuner_seeds, grapher_seeds):
     grapher_seed = grapher_seeds[j]
     caller = IraceCallerBinningWithDefaults(size, j, iterative_seeding_multiples[i][j], previous_lbds, get_iter_bins(size, j+1), tuner_seed)
     caller.run()
-    with open(f"irace_output/binning_with_defaults_{size}_{experiment_multiples_dynamic[i]}_{j}_{tuner_seed}.json", "w") as f:
+    with open(f"irace_output/binning_with_defaults_{size}_{iterative_seeding_multiples[i][j]}_{j}_{tuner_seed}.json", "w") as f:
       json.dump(caller.best_config, f)
     rng = numpy.random.default_rng(grapher_seed)
     previous_lbds = caller.best_config
     print(previous_lbds)
-    load_or_run_binning_comparison_validation(size, f'irace_output/performance_binning_with_defaults_{size}_{experiment_multiples_dynamic[i]}_{j}_{tuner_seed}_{grapher_seed}.json', caller.best_config, next_randoms(rng, trials), pool)
+    load_or_run_binning_comparison_validation(size, f'irace_output/performance_binning_with_defaults_{size}_{iterative_seeding_multiples[i][j]}_{j}_{tuner_seed}_{grapher_seed}.json', caller.best_config, next_randoms(rng, trials), pool)
 
 
 if __name__ == '__main__':
